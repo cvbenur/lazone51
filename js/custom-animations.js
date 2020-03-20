aebc2e6e630cb0e7e47a660f4ce578e50a3a8dc0
Ruben Nabet <rubenabet@gmail.com>
2020-03-20 20:01:48 +0100

    Add dummy video

--- js/custom-animations.js
+++ js/custom-animations.js
@@ -2,6 +2,15 @@ const $ = require('./jquery-3.4.1.min.js');
 
 var on = 0;
 
+// Showing the text on click
+$('#vdc').on('click', function() {
+    $(this).next().removeClass('invisible').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $(this).removeClass('animated fadeIn');
+        $(this).children().eq(0).text("T'as cru !");
+        $(this).children().eq(0).addClass('animated jackInTheBox');
+    });
+});
+
 //  Showing/hiding the "Et pourquoi pas ?" element and changing the value of the button's text
 $('#show-btn').on('click', function() {
     if (on == 0) {
